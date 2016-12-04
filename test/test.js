659391cc56bef7cf70006762598f14e5ba9e87df
Nathan Rajlich <nathan@tootallnate.net>
2016-12-04 10:17:17 -0800

    refactor using `node-buffers` and `ref-struct`

--- test/test.js
+++ test/test.js
@@ -1,30 +1,56 @@
-var InputEvent = require('../');
+const InputEvent = require('../');
 
-// var input = new InputEvent('/dev/input/event0');
-var input = new InputEvent('/dev/input/mice');
-//
-// input.on('data', function(buffer){
-//   console.log(buffer);
-// });
+const KEY_1 = 2
+const KEY_2 = 3
+const KEY_3 = 4
+const KEY_4 = 5
+const KEY_5 = 6
+const KEY_6 = 7
+const KEY_7 = 8
+const KEY_8 = 9
+const KEY_9 = 10
+const KEY_0 = 11
+const KEY_ENTER = 28
 
-// var keyboard = new InputEvent.Keyboard(input);
+const keyboard = new InputEvent.Keyboard(process.argv[2]);
 // keyboard.on('data'    , console.log);
 // keyboard.on('keyup'   , console.log);
 // keyboard.on('keydown' , console.log);
-// keyboard.on('keypress', console.log);
-
-var mouse = new InputEvent.Mouse(input);
-// mouse.on('data'    , console.log);
-// mouse.on('move'    , console.log);
-// mouse.on('wheel'   , console.log);
-// mouse.on('keyup'   , console.log);
-// mouse.on('keydown' , console.log);
-mouse.on('keypress', console.log);
-
-
-// var joystick = new InputEvent.JoyStick(input);
-// joystick.on('data'    , console.log);
-// joystick.on('move'    , console.log);
-// joystick.on('keyup'   , console.log);
-// joystick.on('keydown' , console.log);
-// joystick.on('keypress', console.log);
+let buf = []
+keyboard.on('keypress', (ev) => {
+  switch (ev.code) {
+    case KEY_1:
+      buf.push(1)
+      break
+    case KEY_2:
+      buf.push(2)
+      break
+    case KEY_3:
+      buf.push(3)
+      break
+    case KEY_4:
+      buf.push(4)
+      break
+    case KEY_5:
+      buf.push(5)
+      break
+    case KEY_6:
+      buf.push(6)
+      break
+    case KEY_7:
+      buf.push(7)
+      break
+    case KEY_8:
+      buf.push(8)
+      break
+    case KEY_9:
+      buf.push(9)
+      break
+    case KEY_0:
+      buf.push(0)
+      break
+    case KEY_ENTER:
+      console.log('scanned: %j', buf.join(''))
+      break
+  }
+})
